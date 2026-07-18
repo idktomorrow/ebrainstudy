992a4dd555d4aaf3231350c0d65f2d4f4c284335
선웅제 <swj9675@naver.com>
2026-07-18 12:20:04 +0900

    feat: 게시글/댓글 등록·수정 유효성 검증 추가

--- src/main/java/com/study/comment/dto/request/CommentCreateRequest.java
+++ src/main/java/com/study/comment/dto/request/CommentCreateRequest.java
@@ -1,10 +1,15 @@
 package com.study.comment.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 /**
  * 댓글 등록 요청 DTO
  */
 public record CommentCreateRequest(
+    @NotBlank(message = "작성자는 필수입니다.")
     String writer,
+
+    @NotBlank(message = "내용은 필수입니다.")
     String content
 ) {
 
